2588ce33e49bef3fb205ab9aeb4f557972f8a4bd
Michal Middleton <jafa81@gmail.com>
2023-08-24 14:52:00 -0500

    Fix build

--- build.sh
+++ build.sh
@@ -4,8 +4,4 @@ docker build . \
     --file Dockerfile \
     --tag ${CR_IMAGE_NAME}:latest
 
-CUPS_VER=$(docker run --entrypoint apk --rm ${CR_IMAGE_NAME}:latest list --installed cups 2> /dev/null | cut -d'-' -f2)
-ALPINE_VER=$(grep ' alpine:' Dockerfile| cut -d':' -f2)
-CR_IMAGE_TAG="${CUPS_VER}-$(date +%Y%m%d)-alpine${ALPINE_VER}"
-
-docker tag ${CR_IMAGE_NAME}:latest ${CR_IMAGE_NAME}:${CR_IMAGE_TAG}
+docker tag ${CR_IMAGE_NAME}:latest ${CR_IMAGE_NAME}:$(date +%Y%m%d)
